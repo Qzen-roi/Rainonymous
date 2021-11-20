55c3bddf4e0aeb22d4bb3905e6d8cdf90b95fb64
Rainonymous <92586242+Rainonymous@users.noreply.github.com>
2021-11-20 10:08:07 +0545

    Nepali&English

--- Nep&Engbirthyear.py
+++ Nep&Engbirthyear.py
@@ -2,3 +2,8 @@ Nepali_Birth_Year = input('Nepali_Birth_Year: \n')
 age = 2078 - int (Nepali_Birth_Year )
 print ("""Ranjit's Here this is your real age """)
 print  (age)
+
+English_Year = input('English_Year: \n')
+age = 2021 - int(English_Year)
+print ("""Hello! here's Ranjit and this is your real age""")
+print  (age)
